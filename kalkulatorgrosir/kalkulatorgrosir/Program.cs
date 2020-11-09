5ce95779d7eda2ea9a1de7f84c025c0aa927e80a
Dadi <70506176+dadisetiadi@users.noreply.github.com>
2020-11-09 23:06:42 +0700

    Add files via upload
    C#

---
+++ kalkulatorgrosir/kalkulatorgrosir/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kalkulatorgrosir
+{
+    class Program
+    {
+        static double calculateRetail(double grosir, double kenaikannya)
+        {
+            double cal;
+            return cal = (grosir * kenaikannya / 100) + grosir;
+        }
+        static void Main(string[] args)
+        {
+            double wholesale, markup;
+
+            Console.Write("Masukkan harga grosir: $");
+            wholesale = double.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan beberapa % kenaikannya:");
+            markup = double.Parse(Console.ReadLine());
+
+            Console.Write("Harga ecerannya adalah : $");
+            Console.WriteLine(calculateRetail(wholesale, markup));
+
+            Console.ReadLine();
+
+        }
+    }
+}
